5a78848c255364fb8c595863ee27b91e80c6c2b9
Guilherme Marques <guianselmi.sa@hotmail.com>
2022-03-22 22:51:17 -0300

    Testing

--- CodeSharing/CodeSharing/View/MyWorkspace.xaml.cs
+++ CodeSharing/CodeSharing/View/MyWorkspace.xaml.cs
@@ -19,6 +19,8 @@ namespace CodeSharing.View
         public MyWorkspace()
         {
             InitializeComponent();
+
+            
         }
 
         private void NewBlock(object sender, EventArgs e)
